6e872c56ade71ffc1a354200b77ddb7f4ca908ab
Juwon-Yun <eooll2@daum.net>
2022-03-20 19:02:11 +0900

    Fix: useInput

--- src/components/useInput.tsx
+++ src/components/useInput.tsx
@@ -1,21 +1,40 @@
-import React, { useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 import '../Assets/App.css';
 
 const useInputComponent = ():JSX.Element => { 
     // 기본적으로 Input을 업데이트함
-    const useInput = (initialValue : string) => { 
+    const useInput = (initialValue: string, validator: Function) => { 
         
         const [value, setValue] = useState(initialValue)
 
-        return {value}
-    }   
+        const onChange = (event : ChangeEvent<HTMLInputElement> ) => { 
+            const { target: { value } } = event
 
-    const name = useInput("Mr.Juwon")
+            let willUpdate = true;
+
+            if (typeof validator === "function") { 
+                willUpdate = validator(value)
+            }
+            if (willUpdate) { 
+                setValue(value)
+            }
+        }
+
+        return {value, onChange}  
+    }   
+    
+    const maxLen = (value : string ) => { 
+        return value.length <= 10
+    }
+
+    const name = useInput("Mr", maxLen)
+    const email = useInput("@", maxLen)
 
     return (
         <div className="App">
             <h4>Hello useInput hook</h4>
-            <input placeholder="Name" value={name.value} />
+            <input placeholder="Name" defaultValue={name.value} />
+            <input placeholder="Name" defaultValue={email.value} />
         </div>
     )    
 }
